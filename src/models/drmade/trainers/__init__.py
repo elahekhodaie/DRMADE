b17dd148201f75f88bc4f8552b7ff8b6814b6ced
ellykhodaie <elahekhodaie@yahoo.com>
2020-05-28 10:05:15 +0430

    added to init

--- src/models/drmade/trainers/__init__.py
+++ src/models/drmade/trainers/__init__.py
@@ -1,3 +1,4 @@
 from .ae_pretrainer import RobustAutoEncoderPreTrainer
 from .made_pretrainer import RobustMadePreTrainer
 from .base_trainer import DRMADETrainer
+from .partial_trainer import partialConnectedEncoder
